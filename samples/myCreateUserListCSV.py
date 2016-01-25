592d047dc4573772edef7b0613c17ff633e193c3
Peter A. Knoop <knoop@umich.edu>
2016-01-25 08:40:31 -0500

    TODO: check that directory exists for output file, and that filename is not already in use

--- samples/myCreateUserListCSV.py
+++ samples/myCreateUserListCSV.py
@@ -27,6 +27,7 @@ for role in roles:
     roleLookup[role[u'id']] = role[u'name']
     
 # Designate output file for CSV user list.
+# TODO: Check that directory exists, and that file name isn't already in use.
 outputFile = u'c:/temp/' + agolSubdomain + time.strftime("%Y-%m-%d-%H%M%S",time.gmtime()) + u'.csv'
 with open(outputFile, 'wb') as output:
     
